e9a25eb9392f0a820cf3cacf9fe397bb8fa3f825
onik-nandi <iamoniknandi@gmail.com>
2025-12-09 23:16:51 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@assignment-10.g03zhhb.mongodb.net/?appName=assignment-10`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Hello Dev");
 });
 
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Server is running in ${port}`);
 });
